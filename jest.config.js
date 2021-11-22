e4f0b0d7b9deec957e6950beb41abfd1639bd180
Andrew Mendez <Mendez.AndrewM@gmail.com>
2021-11-22 11:34:58 -0700

    configure dev settings

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+// /* eslint-disable no-undef */
+// const { configure } = require('enzyme');
+// // require('jest-enzyme');
+
+// configure({ adapter: new Adapter() });
+
+module.exports = {
+	collectCoverage: true,
+	coverageReporters: [
+		'json',
+		'html'
+	],
+	collectCoverageFrom: ['src/**/*.js'],
+	moduleDirectories: ['node_modules', 'src', 'test'],
+	// testRegex: '(/__tests__/.*(\\.|/)test|spec))\\.(js?|ts?)$',
+	// setupFiles: ['./__tests__/init.js'],
+	// transform: {
+	// 	'^.+\\.(ts|js)$': 'js-jest'
+	// },
+	globals: {
+		'js-jest': {
+			jsConfig: '<rootDir>/jsconfig.json'
+		}
+	}
+};
